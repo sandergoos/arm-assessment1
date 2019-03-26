c5fe87e4f039b36995b99445d8b6138288d89f0f
Sander Goos <sander.goos@indivirtual.com>
2019-03-26 10:37:21 +0100

    starting over

---
+++ deploymentScriptje.ps1
@@ -0,0 +1,34 @@
+﻿### Define Deployment Variables
+{
+$location = 'West Europe'
+$resourceGroupName = 'sander-arm-template'
+$resourceDeploymentName = 'sander-arm-template-deployment'
+$templatePath = $env:SystemDrive + '\' + 'Projects\arm-assessment1'
+$templateFile = 'template.json'
+$template = $templatePath + '\' + $templateFile
+$templateParameterfile = $templatePath + '\' + 'template.parameter.json'
+$password = "Sander@Passw0rd"
+$securePassword = $password | ConvertTo-SecureString -AsPlainText -Force
+}
+
+### Create Resource Group
+{
+New-AzureRmResourceGroup `
+    -Name $resourceGroupName `
+    -Location $location `
+    -Verbose -Force
+}
+
+### Deploy Resources
+{
+$additionalParameters = New-Object -TypeName Hashtable
+$additionalParameters['vmPrivateAdminPassword'] = $securePassword
+
+New-AzureRmResourceGroupDeployment `
+    -Name $resourceDeploymentName `
+    -ResourceGroupName $resourceGroupName `
+    -TemplateFile $template `
+    -TemplateParameterFile $templateParameterfile
+    -DeploymentDebugLogLevel All `
+    -Verbose -Force
+}
